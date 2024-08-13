1b559e6be944aecae05c7573ebeddede5056f4b0
Vargas <jeanpaulfuentesvargas@gmail.com>
2024-08-13 00:56:06 -0600

    se agrego la base de datos y se corrigio el modelo para poder hacer los insert y guardar datos a la base de datos

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@ $router->setBaseURL('/' . $_ENV['APP_NAME']);
 $router->get('/', [AppController::class,'index']);
 
 $router->get('/producto',[ProductoController::class,'index']);
+$router->get('/API/producto/buscar', [ProductoController::class, 'buscarAPI']);
+$router->post('/API/producto/guardar', [ProductoController::class, 'guardarAPI']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
